bf1df65cc8184fd83fb6d14ac9aed571d19759d0
Osla3721 <79808991+Osla3721@users.noreply.github.com>
2021-03-01 13:06:57 +0300

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -26,7 +26,7 @@ for i in ["falcon1", "falcon9", "falconheavy", "all"]:
     t2 = BashOperator(
         task_id="print_data_" + i, 
         bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
-        params={"rocket": i} # falcon1/falcon9/falconheavy
+        params={"rocket": i}, # falcon1/falcon9/falconheavy
         dag=dag
     )
 
